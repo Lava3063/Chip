ae66a3165bb71b8e444d28456e8977720e78df17
Lava3063 <joshg4569@gmail.com>
2021-07-23 11:25:48 -0700

    Added Purge, Avatar, Userinfo, Serverinfo, and the framework for kicking
    Purge and kick currently can be run by anyone, so be warned. I cant figure out how to make it admin only

--- index.js
+++ index.js
@@ -9,12 +9,50 @@ client.once('ready', () => {
 client.login(token);
 
 client.on('message', message => {
+
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
 	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong! :ping_pong:');
 	} else if (message.content === `${prefix}beep`) {
 		message.channel.send('Boop! Nice to see you HUMAN. :robot:');
+	
+	} else if (command === 'kick') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`Kicked ${taggedUser.username}`);
+
+	} else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ dynamic: true })}>`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({ dynamic: true })}>`;
+		});
+
+		message.channel.send(avatarList);
+
+	} else if (command === 'purge') {
+		const amount = parseInt(args[0]) + 1;
+
+		if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		} else if (amount <= 1 || amount > 100) {
+			return message.reply('you need to input a number between 1 and 99.');
+		}
+
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('there was an error trying to purge messages in this channel');
+		});
 	}
- 	else if (message.content === `${prefix}help`) {
+ 		else if (message.content === `${prefix}help`) {
 
 		message.channel.send({embed: {
 			color: 3447003,
@@ -26,6 +64,8 @@ client.on('message', message => {
 			{ name: "!github", value: "Displays My Github Page" },
 			{ name: "!help", value: "Displays this embed"},
 			{ name: "!invite", value: "Displays bot invite link"},
+			{ name: "!userinfo", value: "Displays info about the user (eg, Name, User ID)"},
+			{ name: "!server", value: "Displays server info (eg, Server name People online"},
 
 			]
 			}
@@ -49,7 +89,10 @@ client.on('message', message => {
 			]
 			}
 			});
+	} else if (message.content === `${prefix}userinfo`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	}
-
 
 });
