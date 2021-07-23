c5b2e76698e9f83578ef8418f007e765099b1c02
Lava3063 <joshg4569@gmail.com>
2021-07-23 10:24:29 -0700

    Fixed one command, added 3 more

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ client.on('message', message => {
 	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong! :ping_pong:');
 	} else if (message.content === `${prefix}beep`) {
-		message.channel.send('Boop! :sdkrobot:');
+		message.channel.send('Boop! :robot:');
 	}
+ 	else if (message.content === `${prefix}help`) {
+		message.channel.send('Here are the commands I take:');
+		message.channel.send('!ping: Pings me and if you get a response, im up. If not, im down');
+		message.channel.send('!beep: Beep Boop, I am a ROBOT. Pleased to meet you, HUMAN');
+		message.channel.send('!Help: Displays this message');
+	}
+	else if (message.content === `${prefix}github`) {
+		message.channel.send('Here is my GitHub page!')
+		message.channel.send('https://github.com/Lava3063/Chip')
+		message.channel.send('Feel free to suggust ideas in the issues section, report an issue, or contribute to development')
+	}
+	else if (message.content === `${prefix}about`) {
+		message.channel.send('Hi! Im chip. Im a discord bot designed to be my creators first project, and for all of you to have fun with me.')
+	}
+
+	
+
 });
